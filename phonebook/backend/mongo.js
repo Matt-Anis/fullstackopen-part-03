f83bd4cdb1e1b9e97662c74fffcb86b69446a05a
Matt Anis <webdev.omis@gmail.com>
2026-04-22 17:45:07 +0100

    3.12: Command-line database

---
+++ phonebook/backend/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://admin:${password}@notesdb.ntx0c91.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NotesDB`
+
+mongoose.set("strictQuery", false);
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+const Contact = mongoose.model("Contact", contactSchema)
+
+mongoose.connect(url, { family: 4 }).then(() => {
+    if(process.argv.length === 5) {
+        const contact = new Contact({
+            name: process.argv[3],
+            number: process.argv[4]
+        })
+
+        contact.save().then(result => {
+            console.log("contact saved!")
+            mongoose.connection.close()
+        })
+    } else {
+        Contact.find({}).then(result => {
+            result.forEach(contact => {
+                console.log(contact)
+            })
+        })
+        mongoose.connection.close()
+    }
+})
