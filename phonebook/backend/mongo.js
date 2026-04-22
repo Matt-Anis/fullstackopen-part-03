712112e26e234fa9ba932300374bafe266498f54
Matt Anis <webdev.omis@gmail.com>
2026-04-22 17:47:53 +0100

    fixed typo + corrected close connection close placement

--- phonebook/backend/mongo.js
+++ phonebook/backend/mongo.js
@@ -5,34 +5,36 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://admin:${password}@notesdb.ntx0c91.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NotesDB`
+const url = `mongodb+srv://admin:${password}@notesdb.ntx0c91.mongodb.net/noteApp?retryWrites=true&w=majority&appName=NotesDB`;
 
 mongoose.set("strictQuery", false);
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
-const Contact = mongoose.model("Contact", contactSchema)
+  name: String,
+  number: String,
+});
+const Contact = mongoose.model("Contact", contactSchema);
 
 mongoose.connect(url, { family: 4 }).then(() => {
-    if(process.argv.length === 5) {
-        const contact = new Contact({
-            name: process.argv[3],
-            number: process.argv[4]
-        })
+  if (process.argv.length === 5) {
+    const contact = new Contact({
+      name: process.argv[3],
+      number: process.argv[4],
+    });
 
-        contact.save().then(result => {
-            console.log("contact saved!")
-            mongoose.connection.close()
-        })
-    } else {
-        Contact.find({}).then(result => {
-            result.forEach(contact => {
-                console.log(contact)
-            })
-        })
-        mongoose.connection.close()
-    }
-})
+    contact.save().then((result) => {
+      console.log("contact saved!");
+      mongoose.connection.close();
+    });
+  } else {
+    Contact.find({}).then((result) => {
+        console.log("phonebook:");
+        
+      result.forEach((contact) => {
+        console.log(contact.name, contact.number);
+      });
+      mongoose.connection.close();
+    });
+  }
+});
