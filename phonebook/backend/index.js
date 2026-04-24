65de23f7c8176a9203602b41331a4f97c360883c
Matt Anis <webdev.omis@gmail.com>
2026-04-24 16:25:57 +0100

    3.13: Phonebook database, step 1

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -1,57 +1,40 @@
+require("dotenv").config();
 const express = require("express");
+const Contact = require("./models/contact");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
 
-app.use(morgan((tokens, req, res) => {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-    JSON.stringify(req.body),
-  ].join(" ");
-}));
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  }),
+);
 app.use(express.json());
-app.use(cors())
-app.use(express.static('dist'))
-
-let contacts = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(cors());
+app.use(express.static("dist"));
 
 app.get("/api/persons", (request, response) => {
-  response.json(contacts);
+  Contact.find({}).then((contacts) => {
+    response.json(contacts);
+  });
 });
 
 app.get("/info", (request, response) => {
   const date = new Date();
-  console.log(date);
-
-  response.send(`<p>Phonebook has info for ${contacts.length} people</p>
+  Contact.find({}).then((contacts) => {
+    response.send(`<p>Phonebook has info for ${contacts.length} people</p>
         <p>${date}</p>`);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
---
+++ phonebook/backend/models/contact.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, {family: 4})
+    .then(result => {
+        console.log('connected!')
+    })
+    .catch(error => {
+        console.log('connection failed', error)
+    })
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject.__v
+        delete returnedObject._id
+    }
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
