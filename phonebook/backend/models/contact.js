8e281ed19491d4a10f531a294d82edabff975868
Matt Anis <webdev.omis@gmail.com>
2026-05-03 12:21:44 +0100

    3.20*: Phonebook database, step 8

--- phonebook/backend/models/contact.js
+++ phonebook/backend/models/contact.js
@@ -15,7 +15,14 @@ const contactSchema = new mongoose.Schema({
         type: String,
         minlength: 3,
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: v => /^\d{2,3}-\d+$/.test(v),
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    },
 })
 
 
